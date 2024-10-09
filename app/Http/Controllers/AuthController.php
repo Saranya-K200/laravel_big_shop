6b0fc39edc4d57bded8e8e73594e44e0c2ab929a
saranya-k200 <saranya101105@gmail.com>
2024-10-09 20:32:51 +0530

    login page added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+
+    public function login(Request $request)
+    {
+        return view('frontend/auth/login');
+    }
     public function forget_password(Request $request)
     {
         return view('frontend/auth/forget_password');
--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,5 @@ Route::get('/',[HomeController::class, 'index'])->name('home.index');
 use App\Http\Controllers\AuthController;
 
 Route::get('/forget_password',[AuthController::class, 'forget_password'])->name('home.forget_password');
+
+Route::get('/login',[AuthController::class, 'login'])->name('home.login');
