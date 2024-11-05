318ba0194b838b2f53a891e0b6bf02d4c699ffb2
saranya-k200 <saranya101105@gmail.com>
2024-11-05 19:44:06 +0530

    laravel asset path fixed for footer

--- resources/views/frontend/layout/footer.blade.php
+++ resources/views/frontend/layout/footer.blade.php
@@ -15,7 +15,7 @@
                                     <button class="btn" type="submit">Subscribe</button>
                                 </form>
                             </div>
-                            <img src="frontend/imgs/banner/banner-9.png" alt="newsletter" />
+                            <img src="{{ asset('frontend/imgs/banner/banner-9.png') }}" alt="newsletter" />
                         </div>
                     </div>
                 </div>
@@ -27,7 +27,7 @@
                     <div class="col-lg-1-5 col-md-4 col-12 col-sm-6 mb-md-4 mb-xl-0">
                         <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay="0">
                             <div class="banner-icon">
-                                <img src="frontend/imgs/theme/icons/icon-1.svg" alt="" />
+                                <img src="{{ asset('frontend/imgs/theme/icons/icon-1.svg') }}" alt="" />
                             </div>
                             <div class="banner-text">
                                 <h3 class="icon-box-title">Best prices & offers</h3>
@@ -38,7 +38,7 @@
                     <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
                         <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay=".1s">
                             <div class="banner-icon">
-                                <img src="frontend/imgs/theme/icons/icon-2.svg" alt="" />
+                                <img src="{{ asset('frontend/imgs/theme/icons/icon-2.svg') }}" alt="" />
                             </div>
                             <div class="banner-text">
                                 <h3 class="icon-box-title">Free delivery</h3>
@@ -49,7 +49,7 @@
                     <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
                         <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay=".2s">
                             <div class="banner-icon">
-                                <img src="frontend/imgs/theme/icons/icon-3.svg" alt="" />
+                                <img src="{{ asset('frontend/imgs/theme/icons/icon-3.svg') }}" alt="" />
                             </div>
                             <div class="banner-text">
                                 <h3 class="icon-box-title">Great daily deal</h3>
@@ -60,7 +60,7 @@
                     <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
                         <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay=".3s">
                             <div class="banner-icon">
-                                <img src="frontend/imgs/theme/icons/icon-4.svg" alt="" />
+                                <img src="{{ asset('frontend/imgs/theme/icons/icon-4.svg') }}" alt="" />
                             </div>
                             <div class="banner-text">
                                 <h3 class="icon-box-title">Wide assortment</h3>
@@ -71,7 +71,7 @@
                     <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
                         <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay=".4s">
                             <div class="banner-icon">
-                                <img src="frontend/imgs/theme/icons/icon-5.svg" alt="" />
+                                <img src="{{ asset('frontend/imgs/theme/icons/icon-5.svg') }}" alt="" />
                             </div>
                             <div class="banner-text">
                                 <h3 class="icon-box-title">Easy returns</h3>
@@ -82,7 +82,7 @@
                     <div class="col-lg-1-5 col-md-4 col-12 col-sm-6 d-xl-none">
                         <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay=".5s">
                             <div class="banner-icon">
-                                <img src="frontend/imgs/theme/icons/icon-6.svg" alt="" />
+                                <img src="{{ asset('frontend/imgs/theme/icons/icon-6.svg') }}" alt="" />
                             </div>
                             <div class="banner-text">
                                 <h3 class="icon-box-title">Safe delivery</h3>
@@ -99,74 +99,74 @@
                     <div class="col">
                         <div class="widget-about font-md mb-md-3 mb-lg-3 mb-xl-0 wow animate__animated animate__fadeInUp" data-wow-delay="0">
                             <div class="logo mb-30">
-                                <a href="index.html" class="mb-15"><img src="frontend/imgs/theme/logo.svg" alt="logo" /></a>
+                                <a href="{{ asset('index.html') }}" class="mb-15"><img src="{{ asset('frontend/imgs/theme/logo.svg') }}" alt="logo" /></a>
                                 <p class="font-lg text-heading">Awesome grocery store website template</p>
                             </div>
                             <ul class="contact-infor">
-                                <li><img src="frontend/imgs/theme/icons/icon-location.svg" alt="" /><strong>Address: </strong> <span>5171 W Campbell Ave undefined Kent, Utah 53127 United States</span></li>
-                                <li><img src="frontend/imgs/theme/icons/icon-contact.svg" alt="" /><strong>Call Us:</strong><span>(+91) - 540-025-124553</span></li>
-                                <li><img src="frontend/imgs/theme/icons/icon-email-2.svg" alt="" /><strong>Email:</strong><span>sale@Nest.com</span></li>
-                                <li><img src="frontend/imgs/theme/icons/icon-clock.svg" alt="" /><strong>Hours:</strong><span>10:00 - 18:00, Mon - Sat</span></li>
+                                <li><img src="{{ asset('frontend/imgs/theme/icons/icon-location.svg') }}" alt="" /><strong>Address: </strong> <span>5171 W Campbell Ave undefined Kent, Utah 53127 United States</span></li>
+                                <li><img src="{{ asset('frontend/imgs/theme/icons/icon-contact.svg') }}" alt="" /><strong>Call Us:</strong><span>(+91) - 540-025-124553</span></li>
+                                <li><img src="{{ asset('frontend/imgs/theme/icons/icon-email-2.svg') }}" alt="" /><strong>Email:</strong><span>sale@Nest.com</span></li>
+                                <li><img src="{{ asset('frontend/imgs/theme/icons/icon-clock.svg') }}" alt="" /><strong>Hours:</strong><span>10:00 - 18:00, Mon - Sat</span></li>
                             </ul>
                         </div>
                     </div>
                     <div class="footer-link-widget col wow animate__animated animate__fadeInUp" data-wow-delay=".1s>
                         <h4 class=" widget-title">Company</h4>
                         <ul class="footer-list mb-sm-5 mb-md-0">
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">Delivery Information</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Terms &amp; Conditions</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                            <li><a href="#">Support Center</a></li>
-                            <li><a href="#">Careers</a></li>
+                            <li><a href="{{ asset('#') }}">About Us</a></li>
+                            <li><a href="{{ asset('#') }}">Delivery Information</a></li>
+                            <li><a href="{{ asset('#') }}">Privacy Policy</a></li>
+                            <li><a href="{{ asset('#') }}">Terms &amp; Conditions</a></li>
+                            <li><a href="{{ asset('#') }}">Contact Us</a></li>
+                            <li><a href="{{ asset('#') }}">Support Center</a></li>
+                            <li><a href="{{ asset('#') }}">Careers</a></li>
                         </ul>
                     </div>
                     <div class="footer-link-widget col wow animate__animated animate__fadeInUp" data-wow-delay=".2s">
                         <h4 class="widget-title">Account</h4>
                         <ul class="footer-list mb-sm-5 mb-md-0">
-                            <li><a href="#">Sign In</a></li>
-                            <li><a href="#">View Cart</a></li>
-                            <li><a href="#">My Wishlist</a></li>
-                            <li><a href="#">Track My Order</a></li>
-                            <li><a href="#">Help Ticket</a></li>
-                            <li><a href="#">Shipping Details</a></li>
-                            <li><a href="#">Compare products</a></li>
+                            <li><a href="{{ asset('#') }}">Sign In</a></li>
+                            <li><a href="{{ asset('#') }}">View Cart</a></li>
+                            <li><a href="{{ asset('#') }}">My Wishlist</a></li>
+                            <li><a href="{{ asset('#') }}">Track My Order</a></li>
+                            <li><a href="{{ asset('#') }}">Help Ticket</a></li>
+                            <li><a href="{{ asset('#') }}">Shipping Details</a></li>
+                            <li><a href="{{ asset('#') }}">Compare products</a></li>
                         </ul>
                     </div>
                     <div class="footer-link-widget col wow animate__animated animate__fadeInUp" data-wow-delay=".3s">
                         <h4 class="widget-title">Corporate</h4>
                         <ul class="footer-list mb-sm-5 mb-md-0">
-                            <li><a href="#">Become a Vendor</a></li>
-                            <li><a href="#">Affiliate Program</a></li>
-                            <li><a href="#">Farm Business</a></li>
-                            <li><a href="#">Farm Careers</a></li>
-                            <li><a href="#">Our Suppliers</a></li>
-                            <li><a href="#">Accessibility</a></li>
-                            <li><a href="#">Promotions</a></li>
+                            <li><a href="{{ asset('#') }}">Become a Vendor</a></li>
+                            <li><a href="{{ asset('#') }}">Affiliate Program</a></li>
+                            <li><a href="{{ asset('#') }}">Farm Business</a></li>
+                            <li><a href="{{ asset('#') }}">Farm Careers</a></li>
+                            <li><a href="{{ asset('#') }}">Our Suppliers</a></li>
+                            <li><a href="{{ asset('#') }}">Accessibility</a></li>
+                            <li><a href="{{ asset('#') }}">Promotions</a></li>
                         </ul>
                     </div>
                     <div class="footer-link-widget col wow animate__animated animate__fadeInUp" data-wow-delay=".4s">
                         <h4 class="widget-title">Popular</h4>
                         <ul class="footer-list mb-sm-5 mb-md-0">
-                            <li><a href="#">Milk & Flavoured Milk</a></li>
-                            <li><a href="#">Butter and Margarine</a></li>
-                            <li><a href="#">Eggs Substitutes</a></li>
-                            <li><a href="#">Marmalades</a></li>
-                            <li><a href="#">Sour Cream and Dips</a></li>
-                            <li><a href="#">Tea & Kombucha</a></li>
-                            <li><a href="#">Cheese</a></li>
+                            <li><a href="{{ asset('#') }}">Milk & Flavoured Milk</a></li>
+                            <li><a href="{{ asset('#') }}">Butter and Margarine</a></li>
+                            <li><a href="{{ asset('#') }}">Eggs Substitutes</a></li>
+                            <li><a href="{{ asset('#') }}">Marmalades</a></li>
+                            <li><a href="{{ asset('#') }}">Sour Cream and Dips</a></li>
+                            <li><a href="{{ asset('#') }}">Tea & Kombucha</a></li>
+                            <li><a href="{{ asset('#') }}">Cheese</a></li>
                         </ul>
                     </div>
                     <div class="footer-link-widget widget-install-app col wow animate__animated animate__fadeInUp" data-wow-delay=".5s">
                         <h4 class="widget-title">Install App</h4>
                         <p class="">From App Store or Google Play</p>
                         <div class="download-app">
-                            <a href="#" class="hover-up mb-sm-2 mb-lg-0"><img class="active" src="frontend/imgs/theme/app-store.jpg" alt="" /></a>
-                            <a href="#" class="hover-up mb-sm-2"><img src="frontend/imgs/theme/google-play.jpg" alt="" /></a>
+                            <a href="{{ asset('#') }}" class="hover-up mb-sm-2 mb-lg-0"><img class="active" src="{{ asset('frontend/imgs/theme/app-store.jpg') }}" alt="" /></a>
+                            <a href="{{ asset('#') }}" class="hover-up mb-sm-2"><img src="{{ asset('frontend/imgs/theme/google-play.jpg') }}" alt="" /></a>
                         </div>
                         <p class="mb-20">Secured Payment Gateways</p>
-                        <img class="" src="frontend/imgs/theme/payment-method.png" alt="" />
+                        <img class="" src="{{ asset('frontend/imgs/theme/payment-method.png') }}" alt="" />
                     </div>
                 </div>
         </section>
@@ -180,22 +180,22 @@
                 </div>
                 <div class="col-xl-4 col-lg-6 text-center d-none d-xl-block">
                     <div class="hotline d-lg-inline-flex mr-30">
-                        <img src="frontend/imgs/theme/icons/phone-call.svg" alt="hotline" />
+                        <img src="{{ asset('frontend/imgs/theme/icons/phone-call.svg') }}" alt="hotline" />
                         <p>1900 - 6666<span>Working 8:00 - 22:00</span></p>
                     </div>
                     <div class="hotline d-lg-inline-flex">
-                        <img src="frontend/imgs/theme/icons/phone-call.svg" alt="hotline" />
+                        <img src="{{ asset('frontend/imgs/theme/icons/phone-call.svg') }}" alt="hotline" />
                         <p>1900 - 8888<span>24/7 Support Center</span></p>
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-6 col-md-6 text-end d-none d-md-block">
                     <div class="mobile-social-icon">
                         <h6>Follow Us</h6>
-                        <a href="#"><img src="frontend/imgs/theme/icons/icon-facebook-white.svg" alt="" /></a>
-                        <a href="#"><img src="frontend/imgs/theme/icons/icon-twitter-white.svg" alt="" /></a>
-                        <a href="#"><img src="frontend/imgs/theme/icons/icon-instagram-white.svg" alt="" /></a>
-                        <a href="#"><img src="frontend/imgs/theme/icons/icon-pinterest-white.svg" alt="" /></a>
-                        <a href="#"><img src="frontend/imgs/theme/icons/icon-youtube-white.svg" alt="" /></a>
+                        <a href="{{ asset('#') }}"><img src="{{ asset('frontend/imgs/theme/icons/icon-facebook-white.svg') }}" alt="" /></a>
+                        <a href="{{ asset('#') }}"><img src="{{ asset('frontend/imgs/theme/icons/icon-twitter-white.svg') }}" alt="" /></a>
+                        <a href="{{ asset('#') }}"><img src="{{ asset('frontend/imgs/theme/icons/icon-instagram-white.svg') }}" alt="" /></a>
+                        <a href="{{ asset('#') }}"><img src="{{ asset('frontend/imgs/theme/icons/icon-pinterest-white.svg') }}" alt="" /></a>
+                        <a href="{{ asset('#') }}"><img src="{{ asset('frontend/imgs/theme/icons/icon-youtube-white.svg') }}" alt="" /></a>
                     </div>
                     <p class="font-sm">Up to 15% discount on your first subscribe</p>
                 </div>
