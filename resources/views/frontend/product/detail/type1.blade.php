9d313ff4483a6c2ac875fbd25e70e472cbaf7468
saranya-k200 <saranya101105@gmail.com>
2024-11-08 19:54:05 +0530

    remove category from product detail

--- resources/views/frontend/product/detail/type1.blade.php
+++ resources/views/frontend/product/detail/type1.blade.php
@@ -574,26 +574,6 @@
                             </div>
                         </div>
                         <div class="col-xl-3 primary-sidebar sticky-sidebar mt-30">
-                            <div class="sidebar-widget widget-category-2 mb-30">
-                                <h5 class="section-title style-1 mb-30">Category</h5>
-                                <ul>
-                                    <li>
-                                        <a href="{{ asset('shop-grid-right.html') }}"> <img src="{{ asset('frontend/imgs/theme/icons/category-1.svg') }}" alt="" />Milks & Dairies</a><span class="count">30</span>
-                                    </li>
-                                    <li>
-                                        <a href="{{ asset('shop-grid-right.html') }}"> <img src="{{ asset('frontend/imgs/theme/icons/category-2.svg') }}" alt="" />Clothing</a><span class="count">35</span>
-                                    </li>
-                                    <li>
-                                        <a href="{{ asset('shop-grid-right.html') }}"> <img src="{{ asset('frontend/imgs/theme/icons/category-3.svg') }}" alt="" />Pet Foods </a><span class="count">42</span>
-                                    </li>
-                                    <li>
-                                        <a href="{{ asset('shop-grid-right.html') }}"> <img src="{{ asset('frontend/imgs/theme/icons/category-4.svg') }}" alt="" />Baking material</a><span class="count">68</span>
-                                    </li>
-                                    <li>
-                                        <a href="{{ asset('shop-grid-right.html') }}"> <img src="{{ asset('frontend/imgs/theme/icons/category-5.svg') }}" alt="" />Fresh Fruit</a><span class="count">87</span>
-                                    </li>
-                                </ul>
-                            </div>
                             <!-- Product sidebar Widget -->
                             <div class="sidebar-widget product-sidebar mb-30 p-30 bg-grey border-radius-10">
                                 <h5 class="section-title style-1 mb-30">New products</h5>
